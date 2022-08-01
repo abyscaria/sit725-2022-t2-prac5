63f852cb8e0e33902cdb8ec25bc0ac5226da8d0e
abyscaria <aby.scaria@gmail.com>
2022-08-02 09:26:33 +1000

    REST API - First code commits
    Rest API - commits

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname+'/public'))
+
+//app.use(express.static(__dirname+"test.html"))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -12,13 +15,37 @@ const addNumbers = (number1, number2) => {
     return result;
 }
 
-app.get("/addTwoNumbers",(req,res) => {
+const dividNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 / num2;
+    return result;
+}
+
+const multNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 * num2;
+    return result;
+}
+
+/*app.get("/addTwoNumbers",(req,res) => {
     var number1 = req.query.number1;
     var number2 = req.query.number2;
     var result = addNumbers(number1,number2)
     res.json({statusCode: 200, data: result, message:"Success"})
-})
+})*/
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "sample.html"));
+  });
+
+app.post("/addTwoNumbers",(req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = addNumbers(number1,number2)
+    res.send({data:result})
+})
 
 var port = process.env.port || 4000;
 
