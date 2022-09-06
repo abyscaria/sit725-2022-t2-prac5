c05683b1ffb97b3448b5c921092a860e49ee0919
abyscaria <aby.scaria@gmail.com>
2022-09-06 22:27:54 +1000

    updated 06sep
    updates

--- server.js
+++ server.js
@@ -1,97 +1,51 @@
-var express = require("express")
-var app = express()
-var cors = require('cors')
-app.use(express.static(__dirname+'/public'))
+let express = require("express");
+let app = express();
+let dbConnect = require("./dbConnect");
+
+//dbConnect.dbConnect()
+//var app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+//const MongoClient = require('mongodb').MongoClient;
+
+// routes
+let projectsRoute = require('./routes/projects')
+
+
+var port = process.env.PORT || 8080;
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(express.static(__dirname + '/public'));
+app.use('/api/projects',projectsRoute)
 
-let projectCollection;
 
-const MdbClient = require('mongodb').MongoClient
+app.get("/test", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
 
-const uri= 'mongodb+srv://<userid>:<pssswd>@cluster0.y9pjz5o.mongodb.net/?retrywrites=true&w=majority'
-
-const client = new MdbClient(uri, {useNewUrlParser: true})
-
-//create collection....
-const createColllection = (collectionName) => {
-    client.connect((err,db) => {
-        projectCollection = client.db().collection(collectionName);
-        if(!err) {
-            console.log('MongoDB Connected')
-        }
-        else {
-            console.log("DB Error: ", err);
-            process.exit(1);
-        }
-    })
-}
-
-//insert project......
-const insertProjects = (project, callback) => {
-    projectCollection.insertOne(project,callback);
-}
-
-//get project....
-const getProjects = (callback) => {
-    projectCollection.find({}).toArray(callback);
-}
-
-app.get('/api/projects',(req,res) => {
-  const cardList = [
-  {
-      title: "Laborgini-Murci-Stick",
-      image: "images/Exotic-Cars-2.jpg",
-      link: "About Exotic-Cars 2",
-      desciption: "This is an exotic Lanborgini : Laborgini-Murci-Stick"
-  },
-  {
-      title: "ACID-GREEN-918-SPYDER-12",
-      image: "images/Exotic-Cars-3.jpg",
-      link: "About Exotic-Cars 3",
-      desciption: "This an Exotic Spyder -  ACID-GREEN-918-SPYDER-12"
-  },
-  {
-      title: "ACID-GREEN-918-SPYDER-12",
-      image: "images/Exotic-Cars-4.jpg",
-      link: "About Exotic-Cars 4",
-      desciption: "This an Exotic Spyder -  ACID-GREEN-918-SPYDER-12"
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
   }
-]
-  res.json({statusCode: 200, data: cardList, message:"Success"})
+  else { res.json({result: result, statusCode: 200}).status(200) } 
 })
 
-// get api...!!
-app.get('/api/projects',(req,res) => {
-    getProjects((err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
-        }
-        else {
-            res.json({statusCode: 200, message:"Success", data: result})
-        }
-    })
-})
+//socket test
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
 
-// post api....
-app.post('/api/projects',(req,res) => {
-    console.log("New Project added", req.body)
-    var newProject = req.body;
-    insertProjects(newProject,(err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
-        }
-        else {
-            res.json({statusCode: 200, message:"Project Successfully added", data: result})
-        }
-    })
-})
+});
 
 
-var port = process.env.port || 5000;
-
-app.listen(port,()=>{
-    console.log("App listening to: "+port)
-    createColllection('exoticCars')
-})
+http.listen(port,()=>{
+  console.log("Listening on port ", port);
+});
