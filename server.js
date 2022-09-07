3223cde454aa44cd54f226c34e7f3b0b43df740d
abyscaria <aby.scaria@gmail.com>
2022-09-07 21:51:23 +1000

    minor

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 let express = require("express");
 let app = express();
-//let dbConnect = require("./dbConnect");
-
+let dbConnect = require("./dbConnect");
 //dbConnect.dbConnect()
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 //const MongoClient = require('mongodb').MongoClient;
-
 // routes
 let projectsRoute = require('./routes/projects')
-
-
 var port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/api/projects',projectsRoute)
-
-
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
-
 app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
   var firstNumber = parseInt(req.params.firstNumber) 
   var secondNumber = parseInt(req.params.secondNumber)
@@ -32,20 +25,16 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
   }
   else { res.json({result: result, statusCode: 200}).status(200) } 
 })
-
 //socket test
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
+  console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
   setInterval(()=>{
     socket.emit('number', parseInt(Math.random()*10));
   }, 1000);
-
 });
-
-
 http.listen(port,()=>{
   console.log("Listening on port ", port);
 });
